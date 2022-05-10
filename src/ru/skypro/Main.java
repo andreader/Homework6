fefd66d85a150966b3d9c70d1839667fb393e4c1
Андрей <79538233369@ya.ru>
2022-05-10 16:18:38 +0500

    6.1

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,25 +3,21 @@ package ru.skypro;
 public class Main {
 
     public static void main(String[] args) {
-        String phone = "+960-415 75-37";
-        phone = phone.replace("-", "");
-        phone = phone.replace(" ", "");
-        phone = phone.replace("+", "");
-        if (phone.length() == 10) {
-//            phone = "7" + phone;
-        } else if (phone.length() > 11) {
-            throw new RuntimeException("The phone is too long!");
-        } else if (phone.length() < 10) {
-            throw new RuntimeException("The phone is too short!");
-        } else if (phone.length() == 11 && phone.charAt(0) != '7') {
-            throw new RuntimeException("The alien inside!!!");
+
+        String firstName = "Ivan";
+        String middleName = "Ivanov";
+        String lastName = "Ivanovich";
+        String fullName = middleName + " " + firstName + " " + lastName;
+        if (firstName.isEmpty() || firstName.isBlank()) {
+            System.out.println("Enter the first name.");
+        } else if (middleName.isEmpty() || middleName.isBlank()) {
+            System.out.println("Enter the middle name.");
+        } else if (lastName.isEmpty() || lastName.isBlank()) {
+            System.out.println("Enter the last name.");
         }
-        System.out.println("phone = " + phone);
-        String expectedPhone = "79604157537";
-        if (phone.equals(expectedPhone)) {
-            System.out.println("Success");
-        } else {
-            System.out.println("Fail");
+        else {
+            System.out.println("fullName = " + fullName);
         }
     }
 }
+
